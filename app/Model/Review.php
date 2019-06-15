c1ac066d11d90b056fc376be5361417626f5d0fe
Aslam Doctor <aslam.doctor@gmail.com>
2019-06-15 21:12:55 +0530

    Relationships created for Products and Reviews

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function Product()
+	{
+		return $this->belongsTo(Product::class);
+	}
 }
